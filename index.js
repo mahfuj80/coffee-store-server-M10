6385e080ede6f0b17a64863d9be84edd1cd0a5f4
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-15 15:27:51 +0600

    delete a user from database

--- index.js
+++ index.js
@@ -101,6 +101,14 @@ async function run() {
       res.send(users);
     });
 
+    // Delete A single User
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
